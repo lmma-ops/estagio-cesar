8e3be7bf188397f905566aa6ed06ccc7d9e33337
lmma-ops <lmma@cesar.org.br>
2025-09-19 09:02:32 -0300

    atualização

---
+++ conftest.py
@@ -0,0 +1,13 @@
+# conftest.py
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver():
+    # Setup: initialize the WebDriver
+    #driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    yield driver
+    # Teardown: close the WebDriver
+    driver.quit()
